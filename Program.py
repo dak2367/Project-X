04d85488ecdcc54a2e05f953c55872f88fdbaf60
Project X <dak2367@mavs.uta.edu>
2026-09-11 18:41:07 -0500

    DATABASE CONNECTION SQL

--- Program.py
+++ Program.py
@@ -1,2 +1,9 @@
+from User import User
+import sys
+import sqlite3
+
 def main():
+    sqlConnection = sqlite3.connect("Database.db")
+    sqlCursor = sqlConnection.cursor()
+    
     print("Hello world")
